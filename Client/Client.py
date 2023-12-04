7cbb6b4cb3de42f0afcd8ff3d17ad24f05989bf8
Rezeq Khader <98508228+rezeqk@users.noreply.github.com>
2023-12-03 20:41:07 -0500

    added get_opcode function added get_file_name_length function added string_to_binary function

--- Client/Client.py
+++ Client/Client.py
@@ -1,5 +1,29 @@
 import socket
 
+def get_opcode (opcode_command):
+    # TODO:  lower capital get GET Get
+    opcode_command = opcode_command.lower()
+    opcode = ""
+    if opcode_command == "put":
+        opcode = "000"
+    elif opcode_command == "get":
+        opcode = "001"
+    elif opcode_command == "change":
+        opcode = "010"
+    elif opcode_command == "summary":
+        opcode = "011"
+    elif opcode_command == "help":
+        opcode = "100"
+    return opcode
+
+def get_filename_length(file_name):
+    filename_length = len(file_name)+1
+    filename_length = format(filename_length, "b").zfill(5)
+    return filename_length
+
+def strings_to_binary(file_name):
+    return ''.join(format(ord(i), '08b') for i in file_name)
+
 
 def run_client():
     # create a socket object
@@ -16,14 +40,21 @@ def run_client():
     while True:
         # input message and send it to the server
         msg = input("Enter message: ")
-        client.send(msg.encode("utf-8")[:1024])
+        inputs = msg.split(" ")
+        opcode = get_opcode(inputs[0])
+        filename_length = get_filename_length(inputs[1])
+        filename_binary = strings_to_binary(inputs[1])
+        request = opcode + filename_length + filename_binary
+        print(request)
+
+        client.send(request.encode("utf-8")[:1024])
 
         # receive message from the server
         response = client.recv(1024)
         response = response.decode("utf-8")
 
         # if server sent us "closed" in the payload, we break out of the loop and close our socket
-        if response.lower() == "closed":
+        if response.lower() == "bye":
             break
 
         print(f"Received: {response}")
--- Server/Server.py
+++ Server/Server.py
@@ -1,6 +1,5 @@
 import socket
 
-
 def run_server():
     # create a socket object
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,18 +18,22 @@ def run_server():
     # accept incoming connections
     client_socket, client_address = server.accept()
     print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
-
+    print("please enter a command you want to execute Type help for more info")
     # receive data from the client
     while True:
         request = client_socket.recv(1024)
         request = request.decode("utf-8") # convert bytes to string
         
+        if request.lower() == "help":
+            client_socket.send("help".encode("utf-8"))
+            opcode = 100
+            
         # if we receive "close" from the client, then we break
         # out of the loop and close the conneciton
         if request.lower() == "bye":
             # send response to the client which acknowledges that the
             # connection should be closed and break out of the loop
-            client_socket.send("closed".encode("utf-8"))
+            client_socket.send("bye".encode("utf-8"))
             break
 
         print(f"Received: {request}")
