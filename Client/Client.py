abd13aab322c61cc13547427b66974298ec88e23
Rezeq Khader <98508228+rezeqk@users.noreply.github.com>
2023-12-06 00:08:51 -0500

    refactor and fixed the function

--- Client/Client.py
+++ Client/Client.py
@@ -6,6 +6,7 @@ arguments = sys.argv
 def get_opcode (opcode_command):
     # TODO:  lower capital get GET Get
     opcode_command = opcode_command.lower()
+    
     opcode = ""
     if opcode_command == "put":
         opcode = "000"
@@ -42,28 +43,29 @@ def run_client():
     print(f"Client listening at : {server_port}")
 
     while True:
-        # input message and send it to the server
         msg = input("Enter message: ")
         inputs = msg.split(" ")
         opcode = get_opcode(inputs[0])
-        filename_length = get_filename_length(inputs[1])
-        filename_binary = strings_to_binary(inputs[1])
-        request = opcode + filename_length + filename_binary
-        print(request)
 
+        if opcode == "100":  # Help command
+            request = opcode
+        else:
+            if len(inputs) < 2:
+                print("Error: Missing filename.")
+                break
+            filename_length = get_filename_length(inputs[1])
+            filename_binary = strings_to_binary(inputs[1])
+            request = opcode + filename_length + filename_binary
+
+        print(request)
         client.send(request.encode("utf-8")[:1024])
 
         # receive message from the server
         response = client.recv(1024)
         response = response.decode("utf-8")
 
-        if response.lower() == "help":
-            print("help")
-            print("put <filename>")
-            print("get <filename>")
-            print("change <filename>")
-            print("summary")
-            print("bye")
+        
+          
         # if server sent us "closed" in the payload, we break out of the loop and close our socket
         if response.lower() == "bye":
             break
