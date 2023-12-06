e6bab7a4dcf02decc878c10436072708912c50ee
Rezeq Khader <98508228+rezeqk@users.noreply.github.com>
2023-12-05 21:17:08 -0500

    added the decoding on the server side

--- Server/Server.py
+++ Server/Server.py
@@ -8,9 +8,7 @@ def run_server():
 
     server_ip = "127.0.0.1"  
     server_port = 8000  
-    # add a comment 
     
-
     # bind the socket to a specific address and port
     server.bind((server_ip, server_port))
     # listen for incoming connections
@@ -24,8 +22,30 @@ def run_server():
     while True:
         request = client_socket.recv(1024)
         request = request.decode("utf-8") # convert bytes to string
+        recieved = list(request)
+        opcode = recieved[0] + recieved[1] + recieved[2]
+        text = ""
+        if opcode == "000":
+            text = "put"
+        elif opcode == "001":
+            text = "get"
+        elif opcode == "010":
+            text = "change"
+        elif opcode == "011":
+            text = "summary"
+        elif opcode == "100":
+            text = "help"
+        print(text)
         
-        
+        bins = recieved[8:]
+        result = ""
+        for i in range(8, len(bins), 8):
+            binc = bins[i:i + 8]
+            list_to_str = ''.join(map(str, binc))
+            num = int(list_to_str, 2)
+            result += chr(num)
+        print(result)
+
 
         if request.lower() == "bye":
           
